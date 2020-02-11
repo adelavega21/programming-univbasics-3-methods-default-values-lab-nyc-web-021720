5ed72400e26bb5a79c91ea97a66679d9767eca79
Andrew de la Vega <andrew21delavega@gmail.com>
2020-02-11 18:02:22 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,5 +7,6 @@
 #end
 
 def meal_choice(veg1, veg2, protein = "meat")
-  p "What a nutritious meal! A plate of #{protein} with #{veg1} and #{veg2}."
+  p "What a nutritious meal!"
+  p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
